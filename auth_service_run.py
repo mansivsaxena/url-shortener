9cbd1b239211567c3ac953b3021e2d0af0049209
Akshit Anchan <69382504+akshitanchan@users.noreply.github.com>
2026-02-19 14:48:45 +0100

    env var passes + run fixed

--- auth_service_run.py
+++ auth_service_run.py
@@ -1,6 +1,8 @@
+import os
 from auth_service import create_auth_app
 
 app = create_auth_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8001, debug=False, use_reloader=False)
+    host = os.getenv("APP_HOST", "127.0.0.1")
+    app.run(host=host, port=8001, debug=False, use_reloader=False)
--- url_shortener_service_run.py
+++ url_shortener_service_run.py
@@ -1,6 +1,8 @@
+import os
 from url_shortener_service import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=False, use_reloader=False)
+    host = os.getenv("APP_HOST", "127.0.0.1")
+    app.run(host=host, port=8000, debug=False, use_reloader=False)
